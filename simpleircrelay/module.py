18501edd6ae29892906b8abf32a9ea1497dbbb9a
BtbN <btbn@btbn.de>
2024-10-31 22:45:41 +0100

    Report more actions

--- simpleircrelay/module.py
+++ simpleircrelay/module.py
@@ -14,23 +14,35 @@ class SimpleIrcRelay(Component):
     def ticket_created(self, ticket):
         if self.message_url is None:
             return
-        base_url = self.config.get("trac", "base_url", "https://unset/")
-        if not base_url.endswith("/"):
-            base_url += "/"
+        base_url = self._base_url()
         msg = f"[newticket] {ticket['reporter']}: Ticket #{ticket.id} ([{ticket['component']}] {ticket['summary']}) created {base_url}ticket/{ticket.id}"
         self._send_msg(msg)
 
     def ticket_changed(self, ticket, comment, author, old_values):
-        pass
+        if self.message_url is None or not comment:
+            return
+        base_url = self._base_url()
+        msg = f"[editedticket] {author or 'unknown'}: Ticket #{ticket.id} ([{ticket['component']}] {ticket['summary']}) updated {base_url}ticket/{ticket.id}"
+        self._send_msg(msg)
 
     def ticket_deleted(self, ticket):
         pass
 
     def ticket_comment_modified(self, ticket, cdate, author, comment, old_comment):
-        pass
+        if self.message_url is None or not comment:
+            return
+        base_url = self._base_url()
+        msg = f"[editedticket] {author or 'unknown'}: Ticket #{ticket.id} ([{ticket['component']}] {ticket['summary']}) updated {base_url}ticket/{ticket.id}"
+        self._send_msg(msg)
 
     def ticket_change_deleted(self, ticket, cdate, changes):
         pass
+
+    def _base_url(self):
+        base_url = self.config.get("trac", "base_url", "https://unset/")
+        if not base_url.endswith("/"):
+            base_url += "/"
+        return base_url
 
     def _send_msg(self, msg):
         try:
